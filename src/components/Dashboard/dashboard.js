c989d6269ca7f87e584ec1ba983686836b4f42d8
Aousaf Rashisd <nabilsakibiram@gmail.com>
2020-02-02 16:20:41 +0600

    New UI Development On Progress

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { faPlus, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faCalendar, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../orders/orders.css'
@@ -27,89 +27,97 @@ class Dashboard extends Component {
     render() {
         return (
             <div id="content">
-                <div id="content">
-                    <div className="row component-header-container">
-                        <h2 className="component-header-title">Dashboard</h2>
-                    </div>
-                    <div className="row leftSpace pt-3">
-                        <div id="dateContainer" className="col-md-3">
-                            <h4>Start date</h4>
-                            <button id="showStartCalendar" className="btn-Blue  btn-date" onClick={(e) => this.DisplayCalendar(e)} style={{ fontSize: '16px', padding: '0px 20px' }}>{moment(this.state.startDate).format("MM-DD-YYYY")}</button>
-                            <div className="topSpace" style={{ display: this.state.showStartCalendar ? 'block' : 'none' }}>
-                                <Calendar id="showStartCalendar" value={this.state.startDate}
-                                    //  onChange={(e) => {this.onChange()}}
-                                    onChange={(e) => { this.onChange(e, 'showStartCalendar'); this.DisplayCalendar('', 'showStartCalendar') }}
-                                />
+                <div className="row leftSpace pt-4">
+                    <div className="box-Container col-md-3">
+                        <h4 className="mb-3">Sales today</h4>
+                        <div className="row box-contents-div p-3">
+                            <div className="contents col-md-2 pt-1">
+                                <span style={{ color: '#15e87b' }}>
+                                    <FontAwesomeIcon className="icon" icon={faArrowUp} />
+                                </span>
+                            </div>
+                            <div className="contents-2 col-md-9">
+                                <h3>$2,909</h3>
                             </div>
                         </div>
-                        <div id="dateContainer" className="col-md-3">
-                            <h4>End date</h4>
-                            <button id="showEndCalendar" className="btn-Blue btn-date" onClick={(e) => this.DisplayCalendar(e)} style={{ fontSize: '16px', padding: '0px 20px' }}>{moment(this.state.endDate).format("MM-DD-YYYY")}</button>
-                            <div className="topSpace" style={{ display: this.state.showEndCalendar ? 'block' : 'none' }}>
-                                <Calendar id="showEndCalendar" value={this.state.startDate}
-                                    onChange={(e) => { this.onChange(e, 'showEndCalendar'); this.DisplayCalendar('', 'showEndCalendar') }}
-                                />
-                            </div>
+                        <div className="progress topSpace">
+                            <div className="progress-bar" role="progressbar" style={{ width: '55%', backgroundColor: '#15e87b' }} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     </div>
-                    {/* <div className="row leftSpace pt-4">
-                        <h4>Showing summary {this.state.summaryFrom} </h4>
-                    </div> */}
-                    <div className="row leftSpace pt-2">
-                        <div className="box-Container col-md-4">
-                            <h4 className="mb-3 box-title">Total sales</h4>
-                            <h4 className="fontBold box-content">0</h4>
-                            <button className="box-button">Show Orders</button>
+
+                    <div className="box-Container col-md-3 leftSpace-wide">
+                        <h4 className="mb-3">Sales this month</h4>
+                        <div className="row box-contents-div p-3">
+                            <div className="contents col-md-2 pt-1">
+                                <span style={{ color: '#15e87b' }}>
+                                    <FontAwesomeIcon className="icon" icon={faArrowUp} />
+                                </span>
+                            </div>
+                            <div className="contents-2 col-md-9">
+                                <h3>$2,909</h3>
+                            </div>
                         </div>
-                        <div id="outOfStockBtn" className="box-Container col-md-3" style={{ marginLeft: '15px' }}>
-                            <h4 id="" className="mb-3 box-title">Total profit</h4>
-                            <h4 className="fontBold box-content">50,000 BDT</h4>
+                        <div className="progress  topSpace">
+                            <div className="progress-bar" role="progressbar" style={{ width: '55%', backgroundColor: '#15e87b' }} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
-                        <div id="outOfStockBtn" className="box-Container col-md-4" style={{ marginLeft: '15px', background: '#ff3b3b' }}>
-                            <h4 id="" className="mb-3 box-title">Total due</h4>
-                            <h4 className="fontBold box-content">10,000 BDT</h4>
-                            <button className="box-button" style={{color: '#ff3b3b'}}>Show due payments</button>
-                        </div>
+                        {/* <button className="box-button-narrow">Show Orders</button> */}
                     </div>
-                    <div className="row leftSpace pt-5">
-                        <h4 className="mb-4">Top 3 products</h4>
-                    </div>
-                    <div className="row leftSpace">
-                        <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
-                            <h4 id="" className="mb-3">Maxima Battery</h4>
-                            <h4 className="fontBold">300 units sold</h4>
-                            <div className="progress topSpace">
-                                <div className="progress-bar bg-info" role="progressbar" style={{width: '100%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+
+                    <div className="box-Container col-md-3 leftSpace-wide">
+                        <h4 className="mb-3">Sales this year</h4>
+                        <div className="row box-contents-div p-3">
+                            <div className="contents col-md-2 pt-1">
+                                <span style={{ color: '#15e87b' }}>
+                                    <FontAwesomeIcon className="icon" icon={faArrowUp} />
+                                </span>
+                            </div>
+                            <div className="contents-2 col-md-9">
+                                <h3>$2,909</h3>
                             </div>
                         </div>
-                        <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
-                            <h4 id="" className="mb-3">Passenger Battery</h4>
-                            <h4 className="fontBold">200 units sold</h4>
-                            <div className="progress topSpace">
-                                <div className="progress-bar bg-info" role="progressbar" style={{width: '80%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
+                        <div className="progress  topSpace">
+                            <div className="progress-bar" role="progressbar" style={{ width: '55%', backgroundColor: '#15e87b' }} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
-                        <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
-                            <h4 className="mb-3">Ultra Battery</h4>
-                            <h4 className="fontBold">140 units sold</h4>
-                            <div className="progress topSpace">
-                                <div className="progress-bar bg-info" role="progressbar" style={{width: '65%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
+                        {/* <button className="box-button-narrow">Show Orders</button> */}
                     </div>
 
                 </div>
-
+                <div className="row leftSpace pt-5">
+                    <h4 className="mb-4">Top 3 products</h4>
+                </div>
+                <div className="row leftSpace">
+                    <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
+                        <h4 id="" className="mb-3">Maxima Battery</h4>
+                        <h4 className="fontBold">300 units sold</h4>
+                        <div className="progress topSpace">
+                            <div className="progress-bar bg-info" role="progressbar" style={{ width: '100%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
+                        <h4 id="" className="mb-3">Passenger Battery</h4>
+                        <h4 className="fontBold">200 units sold</h4>
+                        <div className="progress topSpace">
+                            <div className="progress-bar bg-info" role="progressbar" style={{ width: '80%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div className="top-product-container col-md-3" style={{ marginLeft: '15px' }}>
+                        <h4 className="mb-3">Ultra Battery</h4>
+                        <h4 className="fontBold">140 units sold</h4>
+                        <div className="progress topSpace">
+                            <div className="progress-bar bg-info" role="progressbar" style={{ width: '65%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
             </div >
         );
     }
 
-    async GetSellAndProfit(){
-        const profitSellResponse = await Axios.get( API_FOR_PROD + '/dashboard/GetDashboard?shopId=' + this.state.shopId + '&startdate='+ moment(new Date()).format('1/28/2020') + '&enddate='+ moment(new Date()).format('1/28/2021') )
+    async GetSellAndProfit() {
+        const profitSellResponse = await Axios.get(API_FOR_PROD + '/dashboard/GetDashboard?shopId=' + this.state.shopId + '&startdate=' + moment(new Date()).format('1/28/2020') + '&enddate=' + moment(new Date()).format('1/28/2021'))
         console.log(profitSellResponse)
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.GetSellAndProfit();
     }
     DisplayCalendar(e, keyName) {
